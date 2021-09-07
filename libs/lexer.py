270da394096e6b709ae37391f2f2c355f515bec5
Djonasus <60808389+Djonasus@users.noreply.github.com>
2021-09-07 18:58:43 +0300

    Create lexer.py

---
+++ libs/lexer.py
@@ -0,0 +1,16 @@
+def Lex(line):
+    words = []
+    spec_words = ["="," ",",","[","]","(",")","{","}"]
+    lastln = 0
+    for l in range(len(line)):
+        if line[l] in spec_words:
+            if l == 0:
+                continue
+            else:
+                words.append(line[lastln:l])
+                words.append(line[l])
+                lastln = l+1
+        elif l == len(line)-1:
+            words.append(line[lastln:l+1])
+    words = [x for x in words if x]
+    return words
